2bb46e3b5259836e8a3c6ad39d2b402ef2fd957f
Maria Celeste Coronel <mcelestecrnl@gmail.com>
2023-11-16 21:20:34 -0300

    nombres de variables, funciones

---
+++ src/clean-code/02-name.ts
@@ -0,0 +1,47 @@
+(() => {
+
+    
+    const TemperaturesCelsius = [33.6, 12.34];
+
+    
+    const serverIp = '123.123.123.123';
+    
+    const userList = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+    
+    const emails = userList.map( user => user.email );
+    
+    const canJump = false;
+    const canRun = true;
+    const HasItems = true;
+    const isLoading = false;
+
+    
+    const startTime = new Date().getTime();
+    
+    const endTime = new Date().getTime() - startTime;
+    
+    function getBooks() {
+        throw new Error('Function not implemented.');
+    }
+
+   
+    function getBooksByURL( url: string) {
+        throw new Error('Function not implemented.');
+    }
+    
+   
+    function getSquareArea( side: number ) {
+        throw new Error('Function not implemented.');
+    }
+
+    
+    function printJob() {
+        throw new Error('Function not implemented.');
+    }
+    
+    
+
+
+
+
+})();
